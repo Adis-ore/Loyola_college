d093fd2ef6a4e603232a73729395fdf45b292ad9
Adis-ore <adisoreoluwa@gmail.com>
2026-01-19 15:32:26 +0100

    2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,84 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Layout from './components/Layout';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ScrollToTop from "./components/ScrollToTop";
 
 // Pages
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Directory from './pages/Directory';
-import Support from './pages/Support';
-import News from './pages/News';
-import Executives from './pages/Executives';
-import Jobs from './pages/Jobs';
-import ScrollToTop from './components/ScrollToTop';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Directory from "./pages/Directory";
+import Support from "./pages/Support";
+import News from "./pages/News";
+import Executives from "./pages/Executives";
+import Jobs from "./pages/Jobs";
 
 function App() {
   return (
     <AuthProvider>
       <ScrollToTop />
-      <Router>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/login" element={<Login />} />
 
-          {/* Protected Routes with Layout */}
-          <Route element={<Layout />}>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/directory"
-              element={
-                <ProtectedRoute>
-                  <Directory />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/support"
-              element={
-                <ProtectedRoute>
-                  <Support />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/news"
-              element={
-                <ProtectedRoute>
-                  <News />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/executives"
-              element={
-                <ProtectedRoute>
-                  <Executives />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/jobs"
-              element={
-                <ProtectedRoute>
-                  <Jobs />
-                </ProtectedRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Routes wrapped with Layout */}
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/directory"
+            element={
+              <ProtectedRoute>
+                <Directory />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/support"
+            element={
+              <ProtectedRoute>
+                <Support />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/news"
+            element={
+              <ProtectedRoute>
+                <News />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/executives"
+            element={
+              <ProtectedRoute>
+                <Executives />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/jobs"
+            element={
+              <ProtectedRoute>
+                <Jobs />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
     </AuthProvider>
   );
 }
